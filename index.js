6a4672e9dda35f9298658512a959be3060355265
Daniel Schmidt <danielmschmidt92@gmail.com>
2018-04-13 21:13:33 +0200

    find a budget

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const ynab = require("ynab");
 const accessToken = process.env.ACCESS_TOKEN;
+const budgetName = process.env.BUDGET;
 
 if (!accessToken) {
   throw "Please provide an access token as ACCESS_TOKEN";
+}
+
+if (!budgetName) {
+  throw "Please provide a budget to add the transactions to. You can do this as BUDGET.";
 }
 
 const ynabAPI = new ynab.api(accessToken);
@@ -11,9 +16,14 @@ const ynabAPI = new ynab.api(accessToken);
   try {
     const budgetsResponse = await ynabAPI.budgets.getBudgets();
     const budgets = budgetsResponse.data.budgets;
-    for (let budget of budgets) {
-      console.log(`Budget Name: ${budget.name}`);
+    const selectedBudget = budgets.find(x => x.name === budgetName);
+
+    if (!selectedBudget) {
+        throw `Budget with name '${budgetName}' not found`;
     }
+
+    // TODO: gather transactions from CSV
+    // TODO: add each transaction with ynab API
   } catch(e) {
     console.error(e);
   }
